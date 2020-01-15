6f7c0ef3b691aa81992ce0f259743afa7ab703ad
Willian <willian.vieira@usherbrooke.ca>
2020-01-15 14:17:54 -0500

    :sparkles: get all and new ads R functions

---
+++ R/getAds.R
@@ -0,0 +1,99 @@
+# get adds
+all_KijijiAds = function(URL, exclude, pages = 3, outputFile = NULL)
+{
+  # source python functions
+  reticulate::source_python('python/kijiji_scrap.py')
+
+  # pass adsList to KijijiScraper python function
+  kijiji = KijijiScraper(exclude = exclude)
+
+  # get ads from first page
+  ads = kijiji$scrape_kijiji_for_ads(URL)
+
+  # get ads from other pages if requested
+  if(pages > 1) {
+
+    # get first and last part of URL to add page number
+    firstURL = sub('/[^/]*$', '', URL)
+    lastURL = sub('.*[^/]/', '', URL)
+
+    for(page in 2:pages)
+    {
+      URL = paste0(firstURL, '/page-', page, '/', lastURL)
+      ads <- append(ads, kijiji$scrape_kijiji_for_ads(URL))
+    }
+  }
+
+  # output results
+  if(is.null(outputFile))
+  {
+    return(ads)
+
+  }else{
+    # save ads in the file
+    write(rjson::toJSON(ads), file = outputFile)
+  }
+}
+
+
+
+
+new_KijijiAds = function(URL, exclude, pages, inputFile, outputFile = NULL)
+{
+
+  # source python functions
+  reticulate::source_python('python/kijiji_scrap.py')
+
+  # load ads file if it exists
+  if(file.exists(inputFile)) {
+    adsFile = rjson::fromJSON(file = inputFile)
+    adsNames = as.list(names(adsFile))
+  }else {
+    stop('inputFile is not correct. If you do not have a json file with previews ads, please use the all_KijijiAds() function.')
+  }
+
+  # pass adsList to KijijiScraper python function
+  kijiji = KijijiScraper(exclude = exclude)
+
+  newAds = kijiji$scrape_kijiji_for_ads(URL)
+
+  # get ads from other pages if requested
+  if(pages > 1) {
+
+    # get first and last part of URL to add page number
+    firstURL = sub('/[^/]*$', '', URL)
+    lastURL = sub('.*[^/]/', '', URL)
+
+    for(page in 2:pages)
+    {
+      URL = paste0(firstURL, '/page-', page, '/', lastURL)
+      newAds <- append(newAds, kijiji$scrape_kijiji_for_ads(URL))
+    }
+  }
+
+  # remove ads already present in data base
+  newAds = newAds[!(names(newAds) %in% adsNames)]
+
+  if(length(newAds) == 0) {
+
+    print('Did not find new ads')
+
+  }else {
+
+    print(paste('Found', length(newAds), 'ads. Updating the `', inputFile, '` file'))
+
+    # update data base
+    ads = append(adsFile, newAds)
+    write(rjson::toJSON(ads), file = inputFile)
+
+    # output results
+    if(is.null(outputFile))
+    {
+      return(newAds)
+
+    }else{
+      # save ads in the file
+      write(rjson::toJSON(newAds), file = outputFile)
+    }
+  }
+}
