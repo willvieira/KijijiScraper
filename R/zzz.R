b5b00e37734ec7dc8b3eecad805f24c1e65ab42b
Willian <willian.vieira@usherbrooke.ca>
2020-02-02 20:04:34 -0500

    :bug: try fix python call

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,7 @@ KijijiScraper <- NULL
 
   reticulate::use_python(Sys.which("python3"))
 
-  #the_module <- reticulate::import_from_path(module = "kijiji_scrap", path = system.file("python", package = packageName(), mustWork = TRUE))
-  the_module <<- reticulate::import_from_path("kijiji_scrap", "python")
+  the_module <- reticulate::import_from_path(module = "kijiji_scrap", path = system.file("python", package = packageName(), mustWork = TRUE))
   KijijiScraper <<- the_module$KijijiScraper
 
   invisible()
