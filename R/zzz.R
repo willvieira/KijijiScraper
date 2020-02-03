7b379988b3af431cfda56fb0ecc1ebc814b0eecf
Willian <willian.vieira@usherbrooke.ca>
2020-02-02 20:32:12 -0500

    ➕ better manage python modules

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,25 @@
-KijijiScraper <- NULL
+# from https://community.rstudio.com/t/build-package-namespace-dynamically-during-onload/4101/3
+# Load the module and create dummy objects from it, all of which are NULL
+the_module <- reticulate::import_from_path(
+  "kijiji_scrap",
+  file.path("inst", "python")
+)
+for (obj in names(the_module)) {
+  assign(obj, NULL)
+}
 
+# Clean up
+rm(the_module)
+
+# Now all those names are in the namespace, and ready to be replaced on load
 .onLoad <- function(libname, pkgname) {
+  the_module <- reticulate::import_from_path(
+    "kijiji_scrap",
+    system.file("python", package = utils::packageName())
+  )
 
-  reticulate::use_python(Sys.which("python3"))
-
-  the_module <- reticulate::import_from_path(module = "kijiji_scrap", path = system.file("python", package = packageName(), mustWork = TRUE))
-  KijijiScraper <<- the_module$KijijiScraper
-
-  invisible()
+  # assignInMyNamespace(...) is meant for namespace manipulation
+  for (obj in names(the_module)) {
+    utils::assignInMyNamespace(obj, the_module[[obj]])
+  }
 }
