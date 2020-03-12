85d20ac6232646ee1f813a0d9d0e4608aebd039f
Willian <willian.vieira@usherbrooke.ca>
2020-03-12 09:26:49 -0400

    ✅test sendEmail

--- tests/testthat/test-all_KijijiAds.R
+++ tests/testthat/test-all_KijijiAds.R
@@ -2,9 +2,9 @@ context("test all_KijijiAds function")
 library(KijijiScraper)
 
 # define some variables
-URL = "https://www.kijiji.ca/b-a-louer/ville-de-montreal/3-1-2/k0c30349001l1700281?ll=45.531715%2C-73.593726&address=5500+Rue+St-Hubert%2C+Montr%C3%A9al%2C+QC+H2J+2Y6%2C+Canada&radius=2.0&price=__900"
+URL = "https://www.kijiji.ca/b-audio/ville-de-montreal/turntable/k0c767l1700281?ll=45.501689%2C-73.567256&address=Montr%C3%A9al%2C+QC&radius=15.0"
 pages = 2
-excludeWords = c('echange', 'swap')
+excludeWords = c('mixer')
 
 # get the ads
 ads <- all_KijijiAds(URL, excludeWords, pages, NULL)
---
+++ tests/testthat/test-sendEmail.R
@@ -0,0 +1,17 @@
+context("test sendEmail function")
+library(KijijiScraper)
+
+# laading ads
+ads <- rjson::fromJSON(file = 'ads.json')
+ads <- ads[1]
+
+# loading mailInfo
+download.file(Sys.getenv('mailINFO'), '_mailInfo.yml', method = 'auto', quiet = TRUE)
+
+
+# Testing
+test_that("email is sent", {
+
+  sendEmail(mailInfo = '_mailInfo.yml', newAds = ads)
+
+})
