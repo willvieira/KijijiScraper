75bad02ccb9b9b1dcdc85eedd184a1cd35ca10e1
Willian <willian.vieira@usherbrooke.ca>
2020-03-12 09:42:39 -0400

    ✅fix test associated with exclude :bug:

--- tests/testthat/test-all_KijijiAds.R
+++ tests/testthat/test-all_KijijiAds.R
@@ -4,10 +4,9 @@ library(KijijiScraper)
 # define some variables
 URL = "https://www.kijiji.ca/b-audio/ville-de-montreal/turntable/k0c767l1700281?ll=45.501689%2C-73.567256&address=Montr%C3%A9al%2C+QC&radius=15.0"
 pages = 2
-excludeWords = c('mixer')
 
 # get the ads
-ads <- all_KijijiAds(URL, excludeWords, pages, NULL)
+ads <- all_KijijiAds(URL, exclude = NULL, pages, NULL)
 
 
 # Testing
